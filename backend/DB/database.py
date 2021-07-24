2c265f454f117751994de1b014abcbac7ca33069
ohmono <basura1300@gmail.com>
2021-07-23 21:15:46 -0500

    add DB and its controller

---
+++ backend/DB/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALQUEMY_DB_URL = 'sqlite:///./DB/blog.db'
+
+engine = create_engine(SQLALQUEMY_DB_URL, connect_args={
+                       "check_same_thread": False})
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
