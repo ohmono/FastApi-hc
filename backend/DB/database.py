78533fb88312c4ec54dc1f9f39a89db2f44517ac
ohmono <basura1300@gmail.com>
2021-07-23 21:26:07 -0500

    fix db path

--- backend/DB/database.py
+++ backend/DB/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALQUEMY_DB_URL = 'sqlite:///./DB/blog.db'
+SQLALQUEMY_DB_URL = 'sqlite:///./DB/DB.db'
 
 engine = create_engine(SQLALQUEMY_DB_URL, connect_args={
                        "check_same_thread": False})
